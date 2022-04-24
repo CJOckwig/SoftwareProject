f4b6abf37b7feecb5a63222f088b42d4772eb144
Caleb Ockwig <31769782+CJOckwig@users.noreply.github.com>
2022-04-24 13:01:02 -0500

    Add files via upload

---
+++ SoftwareProject-main/JackrabbitPromotionSoftware/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JackrabbitPromotionSoftware
+{
+    class Customer
+    {
+        public Customer()
+        {
+
+        }
+        public string fName     { get; set; }
+        public string lName     { get; set; }
+        public string phone_Num { get; set; }
+        public bool drinker     { get; set; }
+        public int point        { get; set; }
+
+    }
+}
---
+++ SoftwareProject-main/JackrabbitPromotionSoftware/Reservation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JackrabbitPromotionSoftware
+{
+    class Reservation
+    {
+        public Reservation()
+        {
+
+        }
+        Customer C { get; set; }
+        DateTime ResTime { get; set; }
+        int party_size { get; set; }
+
+    }
+}
